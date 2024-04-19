6603a419229e16d9689ac6bb653398d13a908dd0
Richard Kish <richard.kish1009@gmail.com>
2024-04-19 10:52:21 -0400

    Add files via upload

---
+++ USGS_Lordville.R
@@ -0,0 +1,63 @@
+### USGS Water Data Analysis - Delaware River @ Lordville, New York ###
+### Richard Kish
+### Site ID: 01427207
+
+#Importing USGS Daily Data into RStudio (Delaware @ Lordville)
+usgsDailyData = read.table("https://waterdata.usgs.gov/nwis/dv?cb_00060=on&cb_00065=on&format=rdb&site_no=01427207&referred_module=sw&period&begin_date=1900-01-01&end_date=2024-01-01", header=T, sep= "\t")
+
+#Remove unnecessary top row
+usgsDailyData=usgsDailyData[-c(1),]
+
+#Cleaning usgsDailyData for data used
+usgsDailyData_Filtered = usgsDailyData
+usgsDailyData_Filtered = subset(usgsDailyData_Filtered, select = -c(1,5))
+
+#Change column name for easy scripting
+names(usgsDailyData_Filtered)[3]<-"Mean Discharge [ft3/s]"
+
+#Extract water discharge/flow rate from main dataframe
+chardischarge=as.character(usgsDailyData_Filtered[,3])
+doubledischarge=as.double(chardischarge)
+
+
+###Computing Statistics###
+
+#Initialize and construct statistics matrix
+Mean_Discharge_Stats= matrix(NA, nrow = 1, ncol = 5)
+colnames(Mean_Discharge_Stats)[5] <- "Standard Deviation"
+colnames(Mean_Discharge_Stats)[1] <- "Average"
+colnames(Mean_Discharge_Stats)[2] <- "Median"
+colnames(Mean_Discharge_Stats)[3] <- "Minimum"
+colnames(Mean_Discharge_Stats)[4] <- "Maximum"
+
+#Calculate water flow rate values (avg, median, min, max, std dev)
+mavg=mean(doubledischarge ,na.rm=TRUE)
+mmedian=median(doubledischarge ,na.rm=TRUE)
+mmin=min(doubledischarge ,na.rm=TRUE)
+mmax=max(doubledischarge ,na.rm=TRUE)
+std=sd(doubledischarge ,na.rm=TRUE)
+
+#Affix values to dataframe
+Mean_Discharge_Stats[1,1] = mavg
+Mean_Discharge_Stats[1,2] = mmedian
+Mean_Discharge_Stats[1,3] = mmin
+Mean_Discharge_Stats[1,4] = mmax
+Mean_Discharge_Stats[1,5] = std
+
+print(Mean_Discharge_Stats)
+
+#Initialize and construct flow rate percentiles matrix
+Mean_Discharge_Percentiles= matrix(NA, nrow = 1, ncol = 4)
+colnames(Mean_Discharge_Percentiles)[4] <- "99%"
+colnames(Mean_Discharge_Percentiles)[1] <- "25%"
+colnames(Mean_Discharge_Percentiles)[2] <- "75%"
+colnames(Mean_Discharge_Percentiles)[3] <- "90%"
+names(Mean_Discharge_Percentiles)[1] <- "Mean Discharge [ft3/s]"
+
+#Calculate percentiles from flow rate data
+Mean_Discharge_Percentiles[1,1] = quantile(doubledischarge,0.25,na.rm=T)
+Mean_Discharge_Percentiles[1,2] = quantile(doubledischarge,0.75,na.rm=T)
+Mean_Discharge_Percentiles[1,3] = quantile(doubledischarge,0.90,na.rm=T)
+Mean_Discharge_Percentiles[1,4] = quantile(doubledischarge,0.99,na.rm=T)
+
+print(Mean_Discharge_Percentiles)
---
+++ USGS_WaterAnalysis.R
@@ -0,0 +1,112 @@
+### USGS Water Data Analysis - Tuscolameta Creek @ Walnut Grove, Mississippi
+### Richard Kish
+### Site ID: 02483000
+
+#Importing USGS Daily Data into RStudio
+usgsDailyData=read.table("https://waterdata.usgs.gov/nwis/dv?cb_00060=on&cb_00065=on&format=rdb&site_no=02483000&referred_module=sw&period&begin_date=1900-01-01&end_date=2017-01-01", header=T, sep= "\t")
+
+#Remove unnecessary top row
+usgsDailyData=usgsDailyData[-c(1),]
+
+
+#Cleaning usgsDailyData for data used
+usgsDailyData_Filtered = usgsDailyData
+usgsDailyData_Filtered = subset(usgsDailyData_Filtered, select = -c(1,5,7))
+print(usgsDailyData_Filtered)
+
+#Change names for easier scripting
+names(usgsDailyData_Filtered)[4]<-"Mean Discharge [ft3/s]"
+names(usgsDailyData_Filtered)[3]<-"Mean Stage-height [ft]"
+
+#Extract water discharge/flow rate from main dataframe
+chardischarge=as.character(usgsDailyData_Filtered[,4])
+doubledischarge=as.double(chardischarge)
+
+#Extract water gauge height from main dataframe
+charheight=as.character(usgsDailyData_Filtered[,3])
+doubleheight=as.double(charheight)
+
+
+###Computing Statistics###
+
+#Initialize and construct statistics matrix
+Mean_Discharge_Stats= matrix(NA, nrow = 1, ncol = 5)
+colnames(Mean_Discharge_Stats)[5] <- "Standard Deviation"
+colnames(Mean_Discharge_Stats)[1] <- "Average"
+colnames(Mean_Discharge_Stats)[2] <- "Median"
+colnames(Mean_Discharge_Stats)[3] <- "Minimum"
+colnames(Mean_Discharge_Stats)[4] <- "Maximum"
+
+#Calculate water flow rate values (avg, median, min, max, std dev)
+mavg=mean(doubledischarge ,na.rm=TRUE)
+mmedian=median(doubledischarge ,na.rm=TRUE)
+mmin=min(doubledischarge ,na.rm=TRUE)
+mmax=max(doubledischarge ,na.rm=TRUE)
+std=sd(doubledischarge ,na.rm=TRUE)
+
+#Affix values to dataframe
+Mean_Discharge_Stats[1,1] = mavg
+Mean_Discharge_Stats[1,2] = mmedian
+Mean_Discharge_Stats[1,3] = mmin
+Mean_Discharge_Stats[1,4] = mmax
+Mean_Discharge_Stats[1,5] = std
+
+print(Mean_Discharge_Stats)
+
+
+#Initialize and construct flow rate percentiles matrix
+Mean_Discharge_Percentiles= matrix(NA, nrow = 1, ncol = 4)
+colnames(Mean_Discharge_Percentiles)[4] <- "99%"
+colnames(Mean_Discharge_Percentiles)[1] <- "25%"
+colnames(Mean_Discharge_Percentiles)[2] <- "75%"
+colnames(Mean_Discharge_Percentiles)[3] <- "90%"
+names(Mean_Discharge_Percentiles)[1] <- "Mean Discharge [ft3/s]"
+
+#Calculate percentiles from flow rate data
+Mean_Discharge_Percentiles[1,1] = quantile(doubledischarge,0.25,na.rm=T)
+Mean_Discharge_Percentiles[1,2] = quantile(doubledischarge,0.75,na.rm=T)
+Mean_Discharge_Percentiles[1,3] = quantile(doubledischarge,0.90,na.rm=T)
+Mean_Discharge_Percentiles[1,4] = quantile(doubledischarge,0.99,na.rm=T)
+
+print(Mean_Discharge_Percentiles)
+
+
+###Linear regression analysis on Log-Log scale Mean Discharge [ft3/s] vs Mean Stage-height [ft]
+
+#4.1.1/4.1.2
+usgsDailyData_Filtered_2=usgsDailyData_Filtered
+usgsDailyData_Filtered_2[usgsDailyData_Filtered_2==""]=NA
+usgsDailyData_Filtered_2=na.omit(usgsDailyData_Filtered_2)
+
+#Set Mean Discharge and Stage-height to numeric
+usgsDailyData_Filtered_2$`Mean Discharge [ft3/s]`=as.numeric(as.character(usgsDailyData_Filtered_2$`Mean Discharge [ft3/s]`))
+class(usgsDailyData_Filtered_2$`Mean Discharge [ft3/s]`)
+usgsDailyData_Filtered_2$`Mean Stage-height [ft]`=as.numeric(as.character(usgsDailyData_Filtered_2$`Mean Stage-height [ft]`))
+class(usgsDailyData_Filtered_2$`Mean Stage-height [ft]`)
+
+#Calculate log of water data
+logmeand=log(usgsDailyData_Filtered_2$`Mean Discharge [ft3/s]`)
+logstageh=log(usgsDailyData_Filtered_2$`Mean Stage-height [ft]`)
+usgsDailyData_Filtered_2["LogMeanDischarge"]=logmeand
+usgsDailyData_Filtered_2["LogMeanStageheight"]=logstageh
+
+#Create a linear model using the log-log scale of water data
+lm=lm(logmeand ~ logstageh)
+plot(lm)
+summary(lm)
+
+#Create linear regression line conditions (from summary)
+LinearReg_Intercept=-14.62832
+LinearReg_Slope=7.38662
+LinearReg_RSquared=0.8155
+
+#Setup and visualize discharge vs height (including log-scale)
+par(mfrow = c(1,2))
+plot(usgsDailyData_Filtered_2[,4],usgsDailyData_Filtered_2[,3], xlab="Mean Discharge [ft3/s]",
+     ylab="Mean Stage-height [ft]")
+plot(logmeand,logstageh, xlab="log(Mean Discharge [ft3/s])",
+     ylab="log(Mean Stage-height [ft])")
+
+#Add in linear regression line
+abline(lm(logstageh ~ logmeand), col="red")
+text(x=4, y=3.4, label="Y= 7.38662*X + (-14.62832)")
